0c44e1aa728fdd1d5cc576b6d78c763e6ae856e8
Syed Talha Raza Shah <razatalha70@gmail.com>
2025-09-27 17:12:11 +0500

    Completed the banner of the Figma design.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import product6  from '../assets/products/product6.png'
-import Ellipse27 from '../assets/products/Ellipse27.svg'
-import Ellipse28 from '../assets/products/Ellipse28.svg'
-import Ellipse29 from '../assets/products/Ellipse29.svg'
-import Ellipse30 from '../assets/products/Ellipse30.svg'
+import circle from '../assets/products/circle.png'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
 
 
@@ -13,20 +11,33 @@ const Hero = () => {
       <div className='max-w-full h-[316px] mx-[150px] my-[20px] bg-[#212844] 
       rounded-[16px] flex items-center'>
         <div className='flex flex-row justify-between w-full px-[90px]'>
-          <div>
+          <div className='w-[574px] h-[156px] my-[40px] space-y-[-4px]'>
             <h3 className='text-[30px] text-white font-sans'>Best Deal Online on smart watches</h3>
             <h1 className='text-[63px] font-bold text-white'>SMART WEARABLE.</h1>
             <h3 className='text-[30px] text-white'>UP to 80% OFF</h3>
           </div>
-          <div className="w-[268px] h-[270px]">
-            <img src={Ellipse27} className='my-[-22px]'/>
-
+          <div className="w-[509px] h-[316px] flex items-center justify-end">
+            <img src={circle} alt='circle' className='absolute mx-[-90px] rounded-[16px] object-contain' />
             <img
-              className='relative top-[-50px]'
+              className='w-[268px] h-[270px] relative z-10 right-[30px]'
               src={product6}
               alt="Product"
             />
-          </div>
+            </div>
+          <button className='absolute right-[105px] transform translate-y-[105px] 
+            rounded-full bg-[#F3F9FB] w-[86px] h-[86px] flex items-center justify-center shadow-lg'>
+              <FiChevronRight
+              size={24} 
+              className='text-primary'
+              />
+          </button>
+          <button className='absolute left-[105px] transform translate-y-[105px] 
+            rounded-full bg-[#F3F9FB] w-[86px] h-[86px] flex items-center justify-center shadow-lg'>
+              <FiChevronLeft
+              size={24} 
+              className='text-primary'
+              />
+          </button>
         </div>
       </div>
     </>
